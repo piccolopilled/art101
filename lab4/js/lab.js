b83444f3ec541668c2d2574a2d7d50ffd53f18b3
Kyla Florentino <chewyllamatfm@gmail.com>
2025-04-21 20:39:28 -0700

    created lab 4 page

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Claw Machine
+// An implementation of the game
+// Kyla Florentino <kflorent@ucsc.edu>
+// April 2025
 
-// Constants
+// Display claw machine container
+// Accept user input
+// Validate user input
+	// Insert coin according to player input
+  // Move claw left/right using the joystick according to player input
+  // Drop claw according to player input
+// Run randomized win-lose condition with a 35% win rate
+	// If win:
+		// Play squeak sound effect
+		// Pull claw up with plushie
+	// If lose:
+		// Play creak sound effect
+		// Pull claw up
+// Reset claw to start position
+// If win:
+  // Drop prize
+  // Display win menu with image of plushie		
+// Reset game
 
-// Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
