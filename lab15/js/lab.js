dba2ea800efa5a09d390e54fb995b95874ab70e9
Kyla Florentino <chewyllamatfm@gmail.com>
2025-06-02 17:45:32 -0700

    created lab 15

---
+++ lab15/js/lab.js
@@ -0,0 +1,36 @@
+/*
+Lab 15: AJAX
+Author: Kyla Florentino
+Date: 2 Jun 2025
+*/
+
+const ajaxObj = {
+    url: "https://yesno.wtf/api",
+    // data: {};
+    type: "GET",
+    dataType: "json"
+    
+}
+  
+// when user clicks button
+$("#balls").click(function() {
+    console.log("click");
+    // use jquery ajax to fetch output
+    $.ajax(ajaxObj)
+    // callback for success - chained to ajax
+    .done(function(data) {
+      console.log("Success!");
+      console.log(data);
+      // extract the answer from data
+      let answer = data.answer;
+      let imgURL = data.image;
+      let imageHTML = `<img src='${imgURL}'>`;
+      // insert the output in the output div
+      $("#output").html("<h3>" + answer + "</h3>");
+      $("#output").append(imageHTML);
+    })
+    // callback for failure - chained to ajax
+    .fail(function(xhr, status, errorThrown) {
+      console.log(errorThrown + " Status:" + status );
+    })
+ })
