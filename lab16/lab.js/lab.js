627ef744bf811a0c91fb0931debfaadd24e60872
Kyla Florentino <chewyllamatfm@gmail.com>
2025-06-05 22:19:39 -0700

    updated lab 16

--- lab16/lab.js/lab.js
+++ lab16/lab.js/lab.js
@@ -5,27 +5,32 @@ Date: 5 June 2025
 */
 
 const comicObj = {
-    url: "https://xkcd.com/614/info.0.json",
+    url: "https://corsproxy.io/?https://xkcd.com/info.0.json",
     // data: {};
     type: "GET",
     dataType: "json"
-};
-
-$.ajax(comicObj)
+    
+}
+  
+// when user clicks button
+$("#balls").click(function() {
+    console.log("click");
+    // use jquery ajax to fetch output
+    $.ajax(comicObj)
     // callback for success - chained to ajax
     .done(function(data) {
       console.log("Success!");
       console.log(data);
       // extract the answer from data
       let title = data.title;
-      let text = data.transcript;
       let imgURL = data.img;
       let imageHTML = `<img src='${imgURL}'>`;
       // insert the output in the output div
-      $("#output").html("<h3>" + title + "</h3>" + "<p>" + text + "</p>");
+      $("#output").html("<h3>" + title + "</h3>");
       $("#output").append(imageHTML);
     })
     // callback for failure - chained to ajax
     .fail(function(xhr, status, errorThrown) {
       console.log(errorThrown + " Status:" + status );
     })
+ })
